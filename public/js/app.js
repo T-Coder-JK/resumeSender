193488c8ee73357be1d20f82c1fb5851b998a731
T-Coder-JK <jeffery.wph@gmail.com>
2020-05-31 12:16:27 +1000

    account view modify

--- public/js/app.js
+++ public/js/app.js
@@ -65741,10 +65741,10 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes React and other helpers. It's a great starting point while
- * building robust, powerful web applications using React + Laravel.
- */
+* First we will load all of this project's JavaScript dependencies which
+* includes React and other helpers. It's a great starting point while
+* building robust, powerful web applications using React + Laravel.
+*/
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 /**
  * Next, we will create a fresh React component instance and attach it to
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header back-indigo">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +12,9 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <h5>Applied Positions 20</h5>
 
-                    You are logged in!
+
                 </div>
             </div>
         </div>
